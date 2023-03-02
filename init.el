c19a5de3cbb2e68f0ab68d4f239435dda36a718d
yuys13 <yuys13@users.noreply.github.com>
2023-03-02 18:07:14 +0900

    feat: add highlight-indent-guides

--- init.el
+++ init.el
@@ -48,6 +48,15 @@
 
 (use-package diminish)
 
+(use-package highlight-indent-guides
+  :diminish
+  :hook
+  ((prog-mode yaml-mode) . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-auto-enabled t)
+  (highlight-indent-guides-responsive t)
+  (highlight-indent-guides-method 'character))
+
 (use-package dracula-theme
   :ensure t
   :config
