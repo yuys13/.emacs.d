7f0cd147e2fdbafdce4f271874ecec45db7c32d0
yuys13 <yuys13@users.noreply.github.com>
2023-07-09 00:08:42 +0900

    chore: use column for indent guides instead of char

--- init.el
+++ init.el
@@ -59,7 +59,7 @@
   :custom
   (highlight-indent-guides-auto-enabled t)
   (highlight-indent-guides-responsive t)
-  (highlight-indent-guides-method 'character))
+  (highlight-indent-guides-method 'column))
 
 (use-package dracula-theme
   :config
